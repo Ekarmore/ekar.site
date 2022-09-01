5fb2b82d84aa519403923c70f3fd6cc11ab1c98b
EKar <Earthb0y@outlook.com>
2022-09-01 12:23:31 +0800

    change

--- components/HomePage.vue
+++ components/HomePage.vue
@@ -6,16 +6,16 @@
       <!-- <img class="site-logo" src="/Ekar.png" alt=""> -->
       <p id="p1" class="about-text">
         Hey,我是YEKMAT,来自这颗蓝色星球某一处的氧气呼吸者,心脏跳动者,<a href="https:www.baidu.com" class="about-text-1">前端开发爱好者</a>,<a
-          href="https:www.baidu.com" class="about-text-1">自由摄影师</a>以及<a href="" class="about-text-1">平面设计师.</a></p>
+          href="https:www.baidu.com" class="about-text-1">自由摄影师</a>以及<a href="" class="about-text-1">平面设计师。</a></p>
 
       <p id="p1" class="about-text">
         通过偶然的机会,我开始喜欢上编程并开始学习前端以及试图用编程的方式实现一些有趣的想法,这个网站的创建是其中之一,深受开源精神和互联网分享精神的打动,我也收录了一些自己创造的或是收集到的内容在网站内集中分享以作为享受者的回馈,它们当中有的关于<a
-          class="about-text-1" href="">前端开发</a>,有的关于<a class="about-text-1" href="">设计</a>。
+          class="about-text-1" href="">前端开发</a>,有的关于<a class="about-text-1" href="">设计</a>,希望它们能给你带来一些帮助或者灵感。
       </p>
       <p class="about-text">同时我还很喜欢拍照,如果你也视一些独特的光景为珍宝或仅仅是喜欢拍照,也可以来我的<a class="about-text-1"
           href="">摄影个站</a>看看我在做的一些摄影项目,谈谈你的感受亦或是由于志同道合成为好友,一起讨论摄影和参与一些摄影社群活动
       </p>
-      <p class="about-text">祝你度过完美的一天,再见</p>
+      <p class="about-text">祝你度过完美的一天,再见。</p>
       <hr class="hr-home" />
 
       <div class="link-box">
