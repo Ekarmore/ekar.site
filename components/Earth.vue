7b4a71dd8a8746b80d0fa381018bb932a3faaa50
EKar <Earthb0y@outlook.com>
2022-09-11 13:22:24 +0800

    a

--- components/Earth.vue
+++ components/Earth.vue
@@ -3,7 +3,7 @@
 <!-- <p class="e-title">Our beautiful and only home .</p>  -->
     <main class="earthContainer">
      <iframe class="earth" src='https://my.spline.design/untitled-8a64435d8d1f4464359768064ccb15cb/' />
-     <p class="text-center mt-5 text-sm opacity-60">(双指拖动地球👋)</p>
+     <p class="text-center mt-5 text-sm opacity-60">(双指拖动地球)</p>
      <p class="font-bold text-3xl">Spline</p>
      <hr class="opacity-30 mt-10 mb-10"/>
      <p class="text-lg mb-20">Spline是一个轻量级的3D即时渲染软件</p>
--- components/HomePage.vue
+++ components/HomePage.vue
@@ -6,26 +6,27 @@
       <!-- <img class="site-logo" src="/Ekar.png" alt=""> -->
       <p id="p1" class="about-text">
         Hey,我是YEKMAT,来自这颗蓝色星球某一处的氧气呼吸者,<a href="" class="about-text-1">前端开发爱好者</a>,<a
-          href="" class="about-text-1">自由摄影师</a>以及<a href="" class="about-text-1">平面设计师</a></p>
+          href="" class="about-text-1">自由摄影师</a>。</p>
 
       <p id="p1" class="about-text">
         通过偶然的机会,我开始喜欢上编程并开始学习前端以及试图用编程的方式实现一些有趣的想法,这个网站的创建是其中之一,深受开源精神和互联网分享精神的打动,我也收录了一些自己创造的或是收集到的内容在网站内集中分享以作为享受者的回馈,它们当中有的关于<a
           class="about-text-1" href="">前端开发</a>,有的关于<a class="about-text-1" href="">设计</a>,希望它们能给你带来一些帮助或成为灵感的来源。
       </p>
       <p class="about-text">同时我还很喜欢拍照,如果你也视一些独特的光景为珍宝或仅仅是喜欢拍照,也可以来我的<a class="about-text-1"
-          href="">摄影个站</a>看看我在做的一些摄影项目,谈谈你的感受亦或是由于志同道合成为好友,一起讨论摄影和参与一些摄影社群活动
+          href="">摄影个站</a>看看我在做的一些摄影项目,谈谈你的感受亦或是由于志同道合成为好友,一起讨论摄影和参与一些摄影社群活动。
       </p>
-      <p class="about-text">祝你度过快乐的一天,再见。</p>
       <hr class="hr-home" />
 
       <div class="link-box">
         除了这里,你也可以在
-        <a class="link-foot" href="https://www.instagram.com/ekarmore/">Ins</a> 或
-        <a class="link-foot" href="https://space.bilibili.com/390593000">BiliBili</a> 以及 <a class="link-foot"
-          href="https://github.com/Ekarmore">Github</a> 找到我
+        <a class="link-foot"
+          href="https://github.com/Ekarmore">Github</a>
+        或
+        <a class="link-foot" href="https://space.bilibili.com/390593000">BiliBili</a> 以及 <a class="link-foot" href="https://www.instagram.com/ekarmore/">Ins</a>  找到我
         <br />
         当然,你可以通过邮件与我取得更直接的联系 <a class="link-foot" href="mailto:Earthb0y@outlook.com">Earthb0y@outlook.com</a>
         <br />
+        <p class="about-text">最后,祝你度过快乐的一天,再见。</p>
       </div>
       <p id="p1" class="footer-list">Copyright © 2022-present YEKMAT</p>
     </div>
