814d99e46264ca37b52e0161b9e98d31266c1732
EKar <Earthb0y@outlook.com>
2022-09-11 13:25:35 +0800

    a

--- components/Earth.vue
+++ components/Earth.vue
@@ -3,7 +3,7 @@
 <!-- <p class="e-title">Our beautiful and only home .</p>  -->
     <main class="earthContainer">
      <iframe class="earth" src='https://my.spline.design/untitled-8a64435d8d1f4464359768064ccb15cb/' />
-     <p class="text-center mt-5 text-sm opacity-60">(双指拖动地球)</p>
+     <p class="text-center mt-5 text-sm opacity-60">(使用手指拖动地球)</p>
      <p class="font-bold text-3xl">Spline</p>
      <hr class="opacity-30 mt-10 mb-10"/>
      <p class="text-lg mb-20">Spline是一个轻量级的3D即时渲染软件</p>
